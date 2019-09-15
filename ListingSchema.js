aef28b603fe12bbd6f174d494afa4c8703e0169d
despantoso@ufl.edu <despantoso@ufl.edu>
2019-09-15 19:30:18 -0400

    Passing all mocha test cases.

--- ListingSchema.js
+++ ListingSchema.js
@@ -6,9 +6,20 @@ var mongoose = require('mongoose'),
      See https://mongoosejs.com/docs/guide.html for examples for creating schemas
      See also https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
   */
-var listingSchema = new Schema({
-  /* Your code for a schema here */ 
+var listingSchema = new Schema({ 
   //Check out - https://mongoosejs.com/docs/guide.html
+
+  code: {
+    type:String, required: true
+  },
+  name: {
+    type:String, required: true
+  },
+  coordinates:{
+    latitude: Number,
+    longitude: Number
+  },
+  address: String
 
 });
 
@@ -16,7 +27,14 @@ var listingSchema = new Schema({
    See https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
 */
 listingSchema.pre('save', function(next) {
-  /* your code here */
+  var currentDate = new Date();
+  this.updated_at = currentDate;
+
+  if(!this.created_at){
+    this.created_at = currentDate;
+  }
+  
+  next();
 });
 
 /* Use your schema to instantiate a Mongoose model */
