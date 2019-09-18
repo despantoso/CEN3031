71ceca7e0d0b8eaef600eac463ec01fbf24f60df
despantoso@ufl.edu <despantoso@ufl.edu>
2019-09-18 13:07:31 -0400

    Removed config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-//config.js file to connect the database and hold configuration variables
-
-module.exports = {
-  db: {
-    uri: 'mongodb+srv://despantoso23:Soccerboy03@cluster0-wdbyr.mongodb.net/test?retryWrites=true&w=majority', 
-  }
-};
